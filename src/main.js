4ae0a8b614a01f788f6f8d6a53a1c73a61ce68a5
Arnaiz23 <aaf110501@gmail.com>
2023-08-23 21:16:59 +0200

    feat: ✨ Add JS for open and close the inbox model

--- src/main.js
+++ src/main.js
@@ -4,3 +4,9 @@ const inboxModal = document.querySelector("#inbox-modal")
 inbox.addEventListener("click", () => {
   inboxModal.classList.toggle("header-content-inbox-modal-active")
 })
+
+document.addEventListener("click", (e) => {
+  if (!inbox.contains(e.target) && !inboxModal.contains(e.target)) {
+    inboxModal.classList.remove("header-content-inbox-modal-active")
+  }
+})
